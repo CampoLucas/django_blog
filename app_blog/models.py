865adc71b6e61a6176f0452b12f66a27d188e6ef
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-03-05 14:18:30 -0300

    blog user class

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class BlogUser(AbstractUser):
+    name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+    bio = models.TextField(max_length=300, blank=True)
+    url = models.SlugField(max_length=200, unique=True, editable=False)
+    date_registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id:02}. {self.username.lower()}"
+    
+    def save(self, *args, **kwargs):
+        self.url + slugify(self.username)
+        super().save(*args, **kwargs)
+
